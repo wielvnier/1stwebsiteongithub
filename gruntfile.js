84a6f33406090ab3f80d640fad875d7398649035
Filip Wielanier <filipwielanier@gmail.com>
2017-05-01 12:03:46 +0200

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -42,5 +42,5 @@ module.exports = function(grunt) {
 
   // Default task(s).
 
-  grunt.registerTask('default', ['sass', 'imagemin']);
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
 };
